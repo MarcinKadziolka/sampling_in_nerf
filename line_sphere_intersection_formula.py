aac332bfa847f9883f33f0b2d490e57ec31c7d95
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2023-12-09 16:25:39 +0100

    Delete unused function, add get_intersection_coordinates function

--- line_sphere_intersection_formula.py
+++ line_sphere_intersection_formula.py
@@ -1,28 +1,6 @@
 import torch
 import math
 
-def find_intersections(r, o, u):
-    # finds intersections of a line x = o + du
-    # with a sphere ||x||^2 = r^2 (centered at [0, 0, 0])
-    # in a form of list of values of d
-
-    u_norm = torch.linalg.vector_norm(u).item()
-    u1 = torch.nn.functional.normalize(u, dim=0)   # u1 is an unit vector, same u as u
-
-    delta = (torch.dot(u1, o)**2 - (torch.linalg.vector_norm(o)**2 - r**2)).item()
-
-    if math.isclose(delta, 0, abs_tol=1e-5):    # delta ~ 0
-        # one solution
-        return [-torch.dot(u1, o).item() / u_norm]
-    elif delta < 0:                             # delta < 0
-        # no solutions
-        return []
-    else:                                       # delta > 0
-        # two solutions
-        return [(-torch.dot(u1, o).item() - math.sqrt(delta)) / u_norm,
-                (-torch.dot(u1, o).item() + math.sqrt(delta)) / u_norm]
-    
-
 def line_sphere_intersection(r, o, u):
     a = torch.dot(u, u).item()
     b = 2*( torch.dot(u, o).item())
@@ -36,18 +14,23 @@ def line_sphere_intersection(r, o, u):
     else:  
         return [(-b - math.sqrt(delta)) / (2*a), (-b + math.sqrt(delta)) / (2*a)]
 
-
-
-def b_line_sphere_intersection(r, o, u):
+def batched_line_sphere_intersection(r, o, u):
     # https://discuss.pytorch.org/t/dot-product-batch-wise/9746
     a = (u*u).sum(-1)
     b = 2 * (u*o).sum(-1)
     c = (o*o).sum(-1) - r**2
     delta = b**2 - 4 * a * c
-   # TODO:
+   # Maybe TODO:
    # find indexes of negative delta
    # mask values when delta is negative
 
     points = (-b - torch.sqrt(delta)) / (2*a), (-b + torch.sqrt(delta)) / (2*a)
 
     return torch.stack(points, dim=-1)
+
+def get_intersection_coordinates(o, u, d):
+    intersection_p1 = o + u[:, 0].view(-1, 1) * d
+    intersection_p2 = o + u[:, 1].view(-1, 1) * d
+
+    intersection_points = torch.stack((intersection_p1, intersection_p2), dim=1)
+    return intersection_points
