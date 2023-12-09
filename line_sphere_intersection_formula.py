f9a181f19b9c354ab76c42d5490e58af663b7ece
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2023-12-09 17:09:24 +0100

    Add docstrings

--- line_sphere_intersection_formula.py
+++ line_sphere_intersection_formula.py
@@ -2,6 +2,20 @@ import torch
 import math
 
 def line_sphere_intersection(r, o, u):
+    """Calculate distances of intersections with a sphere
+    Args:
+        r: float. Radius of the sphere        
+        o: array of shape [3]. Coordinates of the ray origin
+        u: array of shape [3]. Vector of the ray direction
+    Returns:
+        Distances of intersections from the ray origin
+        if no intersections:
+            empty array.
+        if one intersection:
+            array of shape [1].
+        if two intersections:
+            array of shape [2]
+    """
     a = torch.dot(u, u).item()
     b = 2*( torch.dot(u, o).item())
     c = torch.dot(o, o).item() - r**2
@@ -15,6 +29,14 @@ def line_sphere_intersection(r, o, u):
         return [(-b - math.sqrt(delta)) / (2*a), (-b + math.sqrt(delta)) / (2*a)]
 
 def batched_line_sphere_intersection(r, o, u):
+    """Calculate batched distances of intersections with a sphere.
+    Args:
+        r: float. Radius of the sphere.        
+        o: array of shape [n_rays, 3]. Coordinates of the rays origins.
+        u: array of shape [n_rays, 3]. Vectors of the rays directions.
+    Returns:
+        array of shape [n_rays, 2]. Distances of intersections from the rays origins.
+    """
     # https://discuss.pytorch.org/t/dot-product-batch-wise/9746
     a = (u*u).sum(-1)
     b = 2 * (u*o).sum(-1)
@@ -23,12 +45,10 @@ def batched_line_sphere_intersection(r, o, u):
    # Maybe TODO:
    # find indexes of negative delta
    # mask values when delta is negative
-
     points = (-b - torch.sqrt(delta)) / (2*a), (-b + torch.sqrt(delta)) / (2*a)
-
     return torch.stack(points, dim=-1)
 
-def get_intersection_coordinates(o, u, d):
+def intersection_coordinates(o, u, d):
     intersection_p1 = o + u[:, 0].view(-1, 1) * d
     intersection_p2 = o + u[:, 1].view(-1, 1) * d
 
