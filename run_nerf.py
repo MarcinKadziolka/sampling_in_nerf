173581261cc893ffcb38883a3b9b26babf6d8d67
ktylus <karaxek23@gmail.com>
2024-01-18 12:22:58 +0100

    removed original points

--- run_nerf.py
+++ run_nerf.py
@@ -358,74 +358,27 @@ def render_rays(ray_batch,
     N_rays = ray_batch.shape[0]
     rays_o, rays_d = ray_batch[:,0:3], ray_batch[:,3:6] # [N_rays, 3] each
 
-    viewdirs = ray_batch[:,-3:] if ray_batch.shape[-1] > 8 else None
-    bounds = torch.reshape(ray_batch[...,6:8], [-1,1,2])
-    near, far = bounds[...,0], bounds[...,1] # [-1,1]
+    ### generating points from the normal distribution
+    # number of points = n_samples
+    intersections = batched_line_sphere_intersection(R, rays_o, rays_d)
+    coordinates = intersection_coordinates(rays_o, rays_d, intersections)
+    mu, sigma = gaussian_nn.forward(coordinates)
+    mu_expanded = mu.expand(-1, N_samples)
+    sigma_expanded = sigma.expand(-1, N_samples)
+    z_vals = torch.normal(mu_expanded, sigma_expanded)
+    ###
 
-    t_vals = torch.linspace(0., 1., steps=N_samples)
-    if not lindisp:
-        z_vals = near * (1.-t_vals) + far * (t_vals)
-    else:
-        z_vals = 1./(1./near * (1.-t_vals) + 1./far * (t_vals))
+    viewdirs = ray_batch[:,-3:] if ray_batch.shape[-1] > 8 else None
 
     z_vals = z_vals.expand([N_rays, N_samples])
 
-    if perturb > 0.:
-        # get intervals between samples
-        mids = .5 * (z_vals[...,1:] + z_vals[...,:-1])
-        upper = torch.cat([mids, z_vals[...,-1:]], -1)
-        lower = torch.cat([z_vals[...,:1], mids], -1)
-        # stratified samples in those intervals
-        t_rand = torch.rand(z_vals.shape)
-
-        # Pytest, overwrite u with numpy's fixed random numbers
-        if pytest:
-            np.random.seed(0)
-            t_rand = np.random.rand(*list(z_vals.shape))
-            t_rand = torch.Tensor(t_rand)
-
-        z_vals = lower + (upper - lower) * t_rand
-
     pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples, 3]
-#     raw = run_network(pts)
     raw = network_query_fn(pts, viewdirs, network_fn)
     rgb_map, disp_map, acc_map, weights, depth_map = raw2outputs(raw, z_vals, rays_d, raw_noise_std, white_bkgd, pytest=pytest)
-
-    if N_importance > 0:
-
-        rgb_map_0, disp_map_0, acc_map_0 = rgb_map, disp_map, acc_map
-
-        z_vals_mid = .5 * (z_vals[...,1:] + z_vals[...,:-1])
-        z_samples = sample_pdf(z_vals_mid, weights[...,1:-1], N_importance, det=(perturb==0.), pytest=pytest)
-        z_samples = z_samples.detach()
-
-        intersections = batched_line_sphere_intersection(R, rays_o, rays_d)
-        coordinates = intersection_coordinates(rays_o, rays_d, intersections)
-        mu, sigma = gaussian_nn.forward(coordinates)
-        N_gaussian_samples = 64
-        mu_expanded = mu.expand(-1, N_gaussian_samples)
-        sigma_expanded = sigma.expand(-1, N_gaussian_samples)
-        gaussian_samples = torch.normal(mu_expanded, sigma_expanded)
-        gaussian_samples[0][0] = 0
-        z_vals = torch.cat((z_vals, gaussian_samples), dim=1)
-
-        z_vals, _ = torch.sort(torch.cat([z_vals, z_samples], -1), -1)
-        pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples + N_importance, 3]
-
-        run_fn = network_fn if network_fine is None else network_fine
-#         raw = run_network(pts, fn=run_fn)
-        raw = network_query_fn(pts, viewdirs, run_fn)
-
-        rgb_map, disp_map, acc_map, weights, depth_map = raw2outputs(raw, z_vals, rays_d, raw_noise_std, white_bkgd, pytest=pytest)
 
     ret = {'rgb_map' : rgb_map, 'disp_map' : disp_map, 'acc_map' : acc_map}
     if retraw:
         ret['raw'] = raw
-    if N_importance > 0:
-        ret['rgb0'] = rgb_map_0
-        ret['disp0'] = disp_map_0
-        ret['acc0'] = acc_map_0
-        ret['z_std'] = torch.std(z_samples, dim=-1, unbiased=False)  # [N_rays]
 
     for k in ret:
         if (torch.isnan(ret[k]).any() or torch.isinf(ret[k]).any()) and DEBUG:
